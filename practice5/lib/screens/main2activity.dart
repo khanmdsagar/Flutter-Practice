c669be51ade42dfc5367bb46fd672a7724d62e38
khanmdsagar <khanmdsagar96@gmail.com>
2022-07-03 20:05:32 +0600

    practice5 - getx

---
+++ practice5/lib/screens/main2activity.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Main2Activity extends StatefulWidget {
+  @override
+  State<Main2Activity> createState() => _Main2ActivityState();
+}
+
+class _Main2ActivityState extends State<Main2Activity> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Second"),),
+    );
+  }
+}
